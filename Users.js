8275a1b89021e5f83b81d1ecec91203411ce29cb
XKolz <samuelayo61@gmail.com>
2022-12-16 04:21:12 -0800

    nodeexpress

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: "John",
+        email: "John@gmail.com",
+    },
+    {
+        id: 2,
+        name: "Smith",
+        email: "Smith@gmail.com",
+    },
+    {
+        id: 3,
+        name: "Jack",
+        email: "Jack@gmail.com",
+    },
+    {
+        id: 4,
+        name: "Chris",
+        email: "Chris@gmail.com",
+    },
+];
+
+module.exports = users;
