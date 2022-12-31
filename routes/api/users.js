ee9b492e8b7a9373a43217f2c9ce1deda6ba8e3b
XKolz <samuelayo61@gmail.com>
2022-12-31 04:03:26 -0800

    Modified code

--- routes/api/users.js
+++ routes/api/users.js
@@ -26,26 +26,44 @@ router.post('/', (req, res) => {
         name: req.body.name,
         email: req.body.email,
     }
+
     if(!newUser.name || !newUser.email) {
         return res.sendStatus(400)
     }
+    
     users.push(newUser)
     res.json(users)
 })
 
 //update user
 router.put('/:id',(req, res) => {
-    const found = users.some(user => user.id === parseInt(req.params.id))
+    const found = users.some((user) => user.id === parseInt(req.params.id))
 
     if(found){
         const updateUser = req.body;
         users.forEach(user =>{
             if(user.id === parseInt(req.params.id)) {
-                user
-            }
-        })
+                user.name = updateUser.name ? updateUser.name: user.name
+                user.email =updateUser.email ? updateUser.email: user.email
+                res.json({msg: 'User updated', user})
+            } 
+        });
     }
-})
+});
 
-module.exports = router
+//delete user
+router.delete("/:id",(req, res) => {
+    const found = users.some((user) => user.id === parseInt(req.params.id));
+
+    if(found){
+        users = users.filter((user) => user.id !== parseInt(req.params.id));
+        res.json({
+            msg: "User deleted",
+            users,
+        });
+    } else {
+        res.sendStatus(400);
+    }
+});
+module.exports = router;
 
